d847fa222791e3e9232b7975627b1a1aa64dfb16
Filip Lipowiecki <fili@softwaremind.com>
2022-01-25 13:39:15 +0100

    added js example and component test

---
+++ BikeShop.UI.Tests/CounterTests.cs
@@ -0,0 +1,35 @@
+﻿using BikeShop.UI.Pages;
+using Bunit;
+using NUnit.Framework;
+
+namespace BikeShop.UI.Tests
+{
+    [TestFixture]
+    internal class CounterTests
+    {
+        [Test]
+        public void CounterShouldIncrementWhenSelected()
+        {
+            // Arrange
+            using var context = new Bunit.TestContext();
+            var counterComponentWrapper = context.RenderComponent<Counter>();
+            var element = counterComponentWrapper.Find("p");
+
+
+            var componentInstance = counterComponentWrapper.Instance;
+
+            var t = componentInstance.currentCountForTest;
+
+            componentInstance.IncrementCountForTest();
+
+            t = componentInstance.currentCountForTest;
+
+            // Act
+            counterComponentWrapper.Find("button").Click();
+            var elementText = element.TextContent;
+
+            // Assert
+            elementText.MarkupMatches("Current count: 1");
+        }
+    }
+}
